005b27a41f2f62175cfd4ed2df9bf0b24eb93780
TylerMorgan142 <tylermorgan142@gmail.com>
2023-06-13 21:20:19 +0000

    .

--- arrow-functions.js
+++ arrow-functions.js
@@ -6,9 +6,30 @@
 // Vanilla JavaScript Function
 
 // Arrow Function With Parameters
+const addTwoNumbers =(a, b) =>{
+    //Code Block
+    return a+b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
-
+const addTwoNumbers2 = (a, b) => a + b;
+sum2 = addTwoNumbers2(6, 4)
+console.log(sum2)
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("Hello there!!");
+
+const sayHello = () => console.log("hello")
+sayHello();
+
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline String
+    </p>`
+)
+console.log(returnMultipleLines());
